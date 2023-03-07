e482cbab9a5f72fdcbf51494451cf8774038868c
Kaukenas Pavel <wiradan@bk.ru>
2023-03-07 18:10:00 +0300

    add-createYear

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { createDate } from './utils/helpers/date/createDate';
-import { createMonth } from './utils/helpers/date/createMonth';
 
 import './styles/App.scss';
-
-console.log(createMonth().createMonthDays());
 
 function App() {
   return <div className="App"></div>;
