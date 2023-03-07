d47aeaade525ad4e33574e3ef2ce05489f1dc46d
Kaukenas Pavel <wiradan@bk.ru>
2023-03-07 17:50:13 +0300

    add-getMonthNumberOfDays

---
+++ src/utils/helpers/date/createMonth.ts
@@ -0,0 +1,32 @@
+import { createDate } from './createDate';
+import { getMonthNumberOfDays } from './getMonthNumberOfDays';
+
+interface CreateMonthParams {
+  locale?: string;
+  date?: Date;
+}
+
+export const createMonth = (params?: CreateMonthParams) => {
+  const date = params?.date ?? new Date();
+  const locale = params?.locale ?? 'default';
+  const d = createDate({ date, locale });
+  const { month: monthName, year, monthNumber, monthIndex } = d;
+  const getDay = (dayNumber: number) =>
+    createDate({ date: new Date(year, monthIndex, dayNumber), locale });
+  const createMonthDays = () => {
+    const days = [];
+    for (let i = 0; i <= getMonthNumberOfDays(monthIndex, year) - 1; i += 1) {
+      days[i] = getDay(i + 1);
+    }
+    return days;
+  };
+
+  return {
+    getDay,
+    monthName,
+    monthNumber,
+    monthIndex,
+    year,
+    createMonthDays,
+  };
+};
